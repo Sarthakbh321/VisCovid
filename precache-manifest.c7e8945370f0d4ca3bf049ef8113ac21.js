faae77518fe2fe8ca7988fb9ffba3b1aaf19cf8a
Sarthak <sarthakbh321@gmail.com>
2020-06-20 12:15:53 +0530

    Updates

---
+++ precache-manifest.c7e8945370f0d4ca3bf049ef8113ac21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a9b646f6c9194adb91c463960e9be6",
+    "url": "/VisCovid/index.html"
+  },
+  {
+    "revision": "916e1dcad621d236cf08",
+    "url": "/VisCovid/static/css/main.d9374119.chunk.css"
+  },
+  {
+    "revision": "99ab078c054e9f4b3856",
+    "url": "/VisCovid/static/js/2.7d614872.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/VisCovid/static/js/2.7d614872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916e1dcad621d236cf08",
+    "url": "/VisCovid/static/js/main.f2d84a3c.chunk.js"
+  },
+  {
+    "revision": "b9afe37eec4f1a264a96",
+    "url": "/VisCovid/static/js/runtime-main.904f5109.js"
+  }
+]);
